5c5e74803d3735c578fc905cc145bc294fb4f90c
(Smitty) Alex Smith <asmit147@ycp.edu>
2016-03-13 17:30:08 -0400

    Git page now has Pubnub publish button that will send a message to all subscribers on channel "hello", proven working, but has js code in html.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
 console.log('This would be the main JS file.');
+
+//src="http://cdn.pubnub.com/pubnub-3.14.3.min.js";
+
+/*function publish() {
+
+    pubnub = Pubnub({
+        publish_key   : 'pub-c-8f6fa682-0e7f-4766-ab33-a00525d8738b',
+        subscribe_key : 'sub-c-db86ff84-e60a-11e5-a25a-02ee2ddab7fe'
+    });
+
+    pubnub.publish({
+        channel : "hello",
+        message : "Hello from PubNub Docs!"})
+
+}*/
