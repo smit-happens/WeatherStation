9e0a84589b9e926908f0b8d0c4ce057b25a7383d
(Smitty) Alex Smith <asmit147@ycp.edu>
2016-03-14 12:02:15 -0400

    User can now send a custom message on the "hello" channel in the input field on the weather station page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,17 +1,30 @@
-//after many failed attempts of only using one .js file, I am only using Pubnub stuff in the html doc and dealing with it
-function testFunc() {
-    var Text = "Literally a test";
+//Channel name for devices to receive from
+var channel = 'hello';
+// Init keys at admin.pubnub.com
+var pubnub = PUBNUB.init({
+    publish_key: 'pub-c-8f6fa682-0e7f-4766-ab33-a00525d8738b',
+    subscribe_key: 'sub-c-db86ff84-e60a-11e5-a25a-02ee2ddab7fe'
+});
 
-    //Channel name for devices to receive from
-    var channel = 'hello';
-    // Init keys at admin.pubnub.com
-    var p = PUBNUB.init({
-        publish_key: 'pub-c-8f6fa682-0e7f-4766-ab33-a00525d8738b',
-        subscribe_key: 'sub-c-db86ff84-e60a-11e5-a25a-02ee2ddab7fe'
-    });
+
+function sendMessage() {
     // Sending data
-    p.publish({
+    var messageInput = document.getElementById("messageInput");
+    var text = messageInput.value;
+
+    pubnub.publish({
         channel: channel,
-        message: Text
+        message: text
     });
+
+    document.getElementById("messageInput").value = "";
 }
+
+function sayHi(){
+    var txtName = document.getElementById("txtName");
+    var txtOutput = document.getElementById("txtOutput");
+    var name = txtName.value;
+    txtOutput.value = "Hi there, " + name + "!"
+}
+
+//function reset() {    document.getElementById("txtField").value = "";}
