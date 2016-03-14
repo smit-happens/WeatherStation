c3d3b8def838e7feea0cf44384195257fa7c21ab
(Smitty) Alex Smith <asmit147@ycp.edu>
2016-03-13 22:03:49 -0400

    The page works with a separate js file referenced :)

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,17 @@
-console.log('This would be the main JS file.');
+//after many failed attempts of only using one .js file, I am only using Pubnub stuff in the html doc and dealing with it
+function testFunc() {
+    var Text = "Literally a test";
 
-//src="http://cdn.pubnub.com/pubnub-3.14.3.min.js";
-
-/*function publish() {
-
-    pubnub = Pubnub({
-        publish_key   : 'pub-c-8f6fa682-0e7f-4766-ab33-a00525d8738b',
-        subscribe_key : 'sub-c-db86ff84-e60a-11e5-a25a-02ee2ddab7fe'
+    //Channel name for devices to receive from
+    var channel = 'hello';
+    // Init keys at admin.pubnub.com
+    var p = PUBNUB.init({
+        publish_key: 'pub-c-8f6fa682-0e7f-4766-ab33-a00525d8738b',
+        subscribe_key: 'sub-c-db86ff84-e60a-11e5-a25a-02ee2ddab7fe'
     });
-
-    pubnub.publish({
-        channel : "hello",
-        message : "Hello from PubNub Docs!"})
-
-}*/
+    // Sending data
+    p.publish({
+        channel: channel,
+        message: Text
+    });
+}
