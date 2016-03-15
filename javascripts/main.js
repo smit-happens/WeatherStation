4061f6feb175a9deed4d960a64e6df0a284f66b8
(Smitty) Alex Smith <asmit147@ycp.edu>
2016-03-15 11:43:03 -0400

    Can now display messages on weather & command channels

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,7 @@
 //Channel name for devices to receive from
-var channel = 'hello';
+var channel1 = 'weather';
+var channel2 = 'command';
+
 // Init keys at admin.pubnub.com
 var pubnub = PUBNUB.init({
     publish_key: 'pub-c-8f6fa682-0e7f-4766-ab33-a00525d8738b',
@@ -7,23 +9,48 @@ var pubnub = PUBNUB.init({
 });
 
 pubnub.subscribe({
-    channel: channel,
-    message: receive //WHY THE FUCK DOES THE FUCKING FUNCTION HAVE TO FUCKING BE CALLED IN THIS BACKWARDS ASS WAY FUCK
+    channel: channel1,
+    callback: receive //WHY THE FUCK DOES THE FUCKING FUNCTION HAVE TO FUCKING BE CALLED IN THIS BACKWARDS ASS WAY FUCK
 });
 
-function receive(txt) {
-    var text = txt.toString();
-    //var channelText = channel.toString();
-    document.getElementById("channelOutput").value += "[" + channel + "]: " + text + "\n";
+pubnub.subscribe({
+    channel: channel2,
+    message: receive
+});
+
+function receive(txt, channel) {
+    var output = document.getElementById("channelOutput");
+
+    if(channel.indexOf("weather") != -1) {
+        output.value += "[weather\t ]: " + txt + "\n";
+    }
+    else {
+        output.value += "[command]: " + txt + "\n";
+    }
+
+    output.scrollTop = output.scrollHeight;
 }
 
-function sendMessage() {
+function sendWeatherMessage() {
     // Sending data
     var messageInput = document.getElementById("messageInput");
     var text = messageInput.value;
 
     pubnub.publish({
-        channel: channel,
+        channel: channel1,
+        message: text
+    });
+
+    document.getElementById("messageInput").value = "";
+}
+
+function sendCommandMessage() {
+    // Sending data
+    var messageInput = document.getElementById("messageInput");
+    var text = messageInput.value;
+
+    pubnub.publish({
+        channel: channel2,
         message: text
     });
 
