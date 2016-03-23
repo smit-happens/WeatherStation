85213426693a2cebdb0d8cfdb5abc65912be5996
Smitty <asmit147@ycp.edu>
2016-03-23 13:14:13 -0400

    adapted photoresistor program to actually publish light level to weather channelusing a loop to average out the light level

--- weatherStation/photoresistorTest.py
+++ weatherStation/photoresistorTest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
-import time, sys, threading, queue
+import time, sys
 from pubnub import Pubnub
 
 pubnub = Pubnub(publish_key="pub-c-8f6fa682-0e7f-4766-ab33-a00525d8738b",
@@ -10,33 +10,28 @@ pubnub = Pubnub(publish_key="pub-c-8f6fa682-0e7f-4766-ab33-a00525d8738b",
 channelW = "weather"
 channelC = "command"
 
-pubnub.subscribe(
-    channelW,
-    callback = callback)
-
-pubnub.subscribe(
-    channelC,
-    callback = callback)
-
 GPIO.setmode(GPIO.BCM)
 
 def RCtime (RCpin):
     reading = 0
     GPIO.setup(RCpin, GPIO.OUT)
     GPIO.output(RCpin, GPIO.LOW)
-    time.sleep(0.2)
+    time.sleep(0.1)
 
     GPIO.setup(RCpin, GPIO.IN)
     while (GPIO.input(RCpin) == GPIO.LOW):
         reading += 1
-    return reading
-
-#usrText = ""
-
-#while usrText != "EXIT":
-#    usrText = raw_input("Enter what channel you would like to use (command or weather [1,2]): ")
-    
+    return reading 
 
 while True:
-    sys.stdout.write("\r%i" % RCtime(18))
-    sys.stdout.flush()
+    lightVal = 0
+    
+    for i in range (0 , 40):
+        lightVal += RCtime(18)
+        
+    lightVal = lightVal/40
+    
+    print('Sending light value of ' + str(lightVal) + ' onto weather channel')
+    text = 'Current light level is ' + str(lightVal)
+    
+    pubnub.publish(channel = channelW, message = text)
