58b33085a4d72d951c333637c5f53d760505c2f6
Smitty <asmit147@ycp.edu>
2016-03-22 23:24:20 -0400

    literally typing this message with vim

--- weatherStation/pi_handshake.py
+++ weatherStation/pi_handshake.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python
-
+#!/usr/bin/python
 
 from pubnub import Pubnub
 
 pubnub = Pubnub(publish_key="pub-c-8f6fa682-0e7f-4766-ab33-a00525d8738b",
                 subscribe_key="sub-c-db86ff84-e60a-11e5-a25a-02ee2ddab7fe")
 
-channel = "hello"
-message = "Literally testing please work"
+message = ""
 
-pubnub.publish(
-    channel = channel,
-    message = message)
+while message != "%exit":
+    channel = "hello"
+    message = raw_input("Enter a message to send: ")
+
+    pubnub.publish(
+        channel = channel,
+        message = message)
+
