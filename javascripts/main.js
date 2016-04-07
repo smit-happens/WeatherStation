4e8ec38a04aa187621bf78f4517f6a87648d96f0
(Smitty) Alex Smith <asmit147@ycp.edu>
2016-04-07 03:38:05 -0400

    Say Hi function is no more

--- javascripts/main.js
+++ javascripts/main.js
@@ -98,10 +98,3 @@ function sendMessage(channel) {
 
     document.getElementById("messageInput").value = "";
 }
-
-function sayHi(){
-    var txtName = document.getElementById("txtName");
-    var txtOutput = document.getElementById("txtOutput");
-    var name = txtName.value;
-    txtOutput.value = "Hi there, " + name + "!"
-}
